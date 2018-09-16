376885bc007705f71ee8c677ac5d052e984d71f2
pedromassango <pedromassango.developer@gmail.com>
2018-09-16 16:28:07 +0100

    reformated code

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'page_three.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
